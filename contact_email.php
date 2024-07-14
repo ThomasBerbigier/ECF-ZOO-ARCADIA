09b286998abf5c141921eeca67e53841ead33afa
Berbigier thomas <thomas.berbigier@gmail.com>
2024-07-14 20:32:26 +0200

    Erreur contact.php

--- contact_email.php
+++ contact_email.php
@@ -8,8 +8,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     
     if(isset($_POST['submitContact'])) {
         
-        $title = filter_input(INPUT_POST,'title', FILTER_VALIDATE_INT);
-        $description = filter_input(INPUT_POST,'description', FILTER_VALIDATE_INT);
+        $title = filter_input(INPUT_POST,'title', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $description = filter_input(INPUT_POST,'description', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $userEmail = filter_input(INPUT_POST,'email', FILTER_SANITIZE_EMAIL);
         
         $mail = new PHPMailer(true);
